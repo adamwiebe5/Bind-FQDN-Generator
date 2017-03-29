feb925a9beb4b680fbe21a99165ff82f1031ce9b
Adam Wiebe <adamwiebe@Adams-iMac.local>
2017-03-29 09:13:15 -0500

    Fully working. Returns FQDN and record type in comma separated format

--- fqdn-generator-bash.sh
+++ fqdn-generator-bash.sh
@@ -138,46 +138,110 @@ zoneContent=`cat $zoneDir'/'$1`
 	    #echo '$INCLUDE'" found! Using zone directory: ""$zoneDir"
 	    zoneFile=`awk '{print $2}' <<< "$z"`
 	    currentZone=`awk '{print $3}' <<< "$z"`
-	    currentZone=${currentZone/%?/}
-	    #echo ${currentZone/%?/}
+	    currentZone=${currentZone%.*}
+	    #echo ${currentZone%.*}
 	    processZone $zoneFile
 	  fi
 
+	  if [[ $z != [0-9][0-9][0-9]* ]]
+	    then
+	    echo 'MATCH'
+	  elif [[ $z == *'TXT'* ]] || [[ $z == *'SPF'* ]] || [[ $z == *'MX'* ]] || [[ $z == *'NS'* ]] || [[ $z == *'SRV'* ]] || [[ $z == *'LOC'* ]]
+	    then
+	    
+	    case $z in
+	      
+	      *'TXT'*)
+		recordType="TXT";;
+
+	      *'SPF'*)
+		recordType="SPF";;
+
+	      *'MX'*)
+		recordType="MX";;
+
+	      *'NS'*)
+		recordType="NS";;
+
+	      *'SRV'*)
+		recordType="SRV";;
+
+	      *'LOC'*)
+		recordType="LOC";;
+
+	      *)
+		recordType="A";;
+	    esac
+#	  else 
+#	    recordType="A"
+	  fi
+	  if [[ $z == *'\$'* ]]
+	    then
+	echo '$GENERATE FOUND'
+	    currentRecord="$GENERATE"
+	  fi
+
 	  currentRecord=`awk '{print $1}' <<< "$z"`
-	  recordType=`awk '{print $1}' <<< "$z"`
 
 	  case $currentRecord in
 
             '$ORIGIN')
-              currentZone="${z/\$ORIGIN /}"
-	      currentZone=${currentZone/%?/};;
+	      if [[ $z == '$ORIGIN '* ]]
+		then
+		currentZone="${z/\$ORIGIN /}"
+	      elif [[ $z == '$ORIGIN	'* ]]
+		then
+		currentZone="${z/\$ORIGIN	/}"
+	      fi
+	      currentZone=${currentZone%.*};;
 
 	    *'.') 
-	      lastFQDN=`echo ${currentRecord/%?/}`
-	      printf "${currentRecord/%?/}\n";;
+	      lastFQDN=`echo ${currentRecord%?.*}`
+	      printf "${currentRecord%.*}","$recordType\n";;
 
-            '$TTL'|'$GENERATE'*|[0-9][0-9][0-9]*|'"'*|'4W')
+            '$TTL'|'$GENERATE'*|'IN'|[0-9][0-9][0-9]*|'"'*|'4W'|'2h)')
               echo "EXCLUDING: ""$currentRecord" > /dev/null;;
 
             '@')
 	      lastFQDN="$currentZone"
-	      printf "$currentZone\n";;
+	      printf "$currentZone","$recordType\n";;
 
-            'TXT'|'SPF'|'A'|'CNAME'|'MX'|'NS')
+            'TXT'|'SPF'|'A'|'CNAME'|'DNAME'|'MX'|'NS'|'LOC'|'SRV')
+	      recordType=`awk '{print $1}' <<< "$z"`
 	      printf "$lastFQDN","$recordType\n";;
 
 	    '' )
 	      echo "THIS IS A BLANK RECORD, using ""$lastFQDN" > /dev/null;;
 
-#	    [^a-zA-Z0-9-]*)
-#	      lastRecord=$currentRecord;;
-#	      #echo "LAST RECORD: ""$lastRecord";;
-	    
 	    *)
 	      lastFQDN="$currentRecord"'.'"$currentZone"
-	      printf "$currentRecord"'.'"$currentZone\n";;
+
+#	      if [[ $currentZone == *'in-addr.arpa'* ]]
+#  		then
+#  		modRecord="$currentRecord"'.'"$currentZone"
+#  		modRecord="${modRecord/.in-addr.arpa/}"
+#
+#  		OIFS=$IFS
+#  		IFS=. read w x y z <<<"$modRecord"
+#  		printf "$z"'.'"$y"'.'"$x"'.'"$w\n"
+#  		IFS=$OIFS
+#	      elif [[ $currentZone == *'IN-ADDR.ARPA'* ]]
+#  		then
+#  		modRecord="$currentRecord"'.'"$currentZone"
+#  		modRecord="${modRecord/.IN-ADDR.ARPA/}"
+#
+#  		OIFS=$IFS
+#  		IFS=. read w x y z <<<"$modRecord"
+#  		printf "$z"'.'"$y"'.'"$x"'.'"$w\n"
+#  		IFS=$OIFS
+#	      else
+ 		printf "$currentRecord"'.'"$currentZone","$recordType\n";;
+#	      fi;;
+
 	  esac
-	      
+
+	unset recordType
+	     
 	fi
       done <<< "$zoneContent"
     unset currentZone
